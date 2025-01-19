b6930a0d9ea5cfb9d538126fecad34642d81862a
satoru <rhodeus208@gmail.com>
2025-01-19 16:09:12 +0900

    streamの練習

--- daily-exercises/2025-01-19-1/src/Main.java
+++ daily-exercises/2025-01-19-1/src/Main.java
@@ -1,5 +1,20 @@
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class Main {
     public static void main(String[] args) {
+
         System.out.println("Hello, World!");
+
+
+    }
+
+    public List<Integer> collectionManipulation(List<Integer> list){
+        return list.stream()
+                .distinct()
+                .filter(num->num%2 ==0)
+                .sorted(Comparator.comparingInt(num->num))
+                .toList();
     }
 }
