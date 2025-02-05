253fbe8b9a5e8cc07a0421ceadd163c738f9c4b1
satoru <rhodeus208@gmail.com>
2025-02-05 12:33:29 +0900

    一応完成

--- daily-exercises/20250205/src/ProductCodeFormatter.java
+++ daily-exercises/20250205/src/ProductCodeFormatter.java
@@ -2,8 +2,10 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class ProductCodeFormatter {
-    private final static String VALID_FORMAT = "^[A-Z]{2}-[0-9]{3}-[A-Z]{2}";
+    private final static String VALID_FORMAT = "^[A-Z]{2}-[0-9]{3}-[A-Z]{2}$";
+    private final static String NEARLY_VALID_FORMAT="^[A-Z]{2}[0-9]{3}[A-Z]{2}$";
     private final static Pattern pattern = Pattern.compile(VALID_FORMAT);
+    private final static Pattern nearPattern = Pattern.compile(NEARLY_VALID_FORMAT);
 
     // 商品コードが正しい形式かチェック
     public static boolean isValidFormat(String code) {
@@ -13,7 +15,27 @@ public class ProductCodeFormatter {
     }
 
     // 商品コードを正しい形式に変換
-//    public static String formatCode(String code) {
-//        // 実装してください
-//    }
+    public static String formatCode(String code) {
+        // 実装してください
+        if(ProductCodeFormatter.isValidFormat(code)){
+            return code;
+        }else{
+            String formatted = code.replaceAll("\\s+","").toUpperCase();
+            if(ProductCodeFormatter.isValidFormat(formatted)){
+                return formatted;
+            }else{
+                String twiceFormatted = formatted.replaceAll("[^A-Z0-9]","");
+                Matcher matcher = nearPattern.matcher(twiceFormatted);
+                if(matcher.matches()){
+                    StringBuilder sb = new StringBuilder(twiceFormatted);
+                    sb.insert(3,"-");
+                    sb.insert(6,"-");
+                    return sb.toString();
+                }
+            }
+
+
+            }
+        return null;
+    }
 }
